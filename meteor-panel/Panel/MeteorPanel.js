08a9a38e5cfef197b6c134e4812471de8d973dad
David Mihal <eastsideski@gmail.com>
2014-11-04 14:57:17 -0500

    Extension can sniff client Collections

--- meteor-panel/Panel/MeteorPanel.js
+++ meteor-panel/Panel/MeteorPanel.js
@@ -10,10 +10,64 @@
   // "background pages."
 
   chrome.devtools.inspectedWindow.eval(
-    'Meteor.release',
+    '({release: Meteor.release})',
     function (result, isException) {
-      document.body.appendChild(document.createTextNode(
-        'Meteor.release = ' + result));
+      if (result.release){
+        document.body.appendChild(document.createTextNode(
+          'Meteor.release = ' + result.release));
+      } else {
+        document.body.appendChild(document.createTextNode("This page does not use Meteor"));
+      }
     });
+
+
+  var injected = function(){
+    var _meteor;
+    window._meteorCollections = {};
+    // Function that does subclassing
+    var __extends = function(child, parent) {
+      for (var key in parent) {
+        if (Object.prototype.hasOwnProperty.call(parent, key)) {
+          child[key] = parent[key];
+        }
+      }
+      function ctor() { this.constructor = child; }
+      ctor.prototype = parent.prototype;
+      child.prototype = new ctor;
+      child.__super__ = parent.prototype;
+      return child;
+    };
+    var __watch = function(name, callback) {
+      var obj = {};
+      Object.defineProperty(window, name, {
+        get: function () { return obj; },
+        set: function (val) {
+          obj = val;
+          callback(obj);
+        }
+      });
+    };
+
+    var initializeMeteor = function(){
+      var MyCollection = function(name, options){
+        MyCollection.__super__.constructor.apply(this, arguments);
+        console.log('collection',arguments,this);
+        _meteorCollections[name] = this;
+      };
+      __extends(MyCollection, Meteor.Collection);
+      Meteor.Collection = MyCollection;
+
+    };
+    __watch("Meteor", initializeMeteor);
+    
+  };
+
+  var inject = function()
+  {
+    chrome.devtools.inspectedWindow.reload({
+      injectedScript : "(" + injected.toString() + ")()"
+    });
+  }
+  document.getElementById('reload').addEventListener('click', inject);
 
 })();
